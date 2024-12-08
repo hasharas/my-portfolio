b9197e2abe66f57d282edcd0b901f58bc8acaa50
hasharas <hasharamsankalpam@gmail.com>
2024-12-09 02:00:37 +0530

    Add : hamberger menu add nav bar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import style from "./Navbar.module.css"
+import React, { useState } from 'react';
+import style from "./Navbar.module.css";
+// import { getImageUrl } from "../../utils";
+import menuIcon from "../../assets/nav/menuIcon.png";
+import closeIcon from "../../assets/nav/closeIcon.png"
+
+
 
 const Navbar = () => {
+      const [menuOpen, setMenuOpen] = useState(false);
       return (
             <nav className={style.navbar} >
 
                   <a className={style.title} href="/">Portfolio</a>
 
                   <div className={style.menu}>
-                        <ul className={style.menuItem}>
+
+                        <img className={style.menuBtn}
+                              src={menuOpen
+                                    ? closeIcon
+                                    : menuIcon}
+                              alt="menu-button"
+                              onClick={() => setMenuOpen(!menuOpen)} />
+
+
+                        <ul className={`${style.menuItem} ${menuOpen && style.menuOpen}`}
+                              onClick={() => setMenuOpen(false)}>
                               <li >
                                     <a href="#about">About</a>
                               </li>
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-export const getImageUrl = () => {
-      return new URL(`assets/${path}`, import.meta.url).href;
+export const getImageUrl = (path) => {
+      return new URL(`/assets/${path}`, import.meta.url).href;
 };
