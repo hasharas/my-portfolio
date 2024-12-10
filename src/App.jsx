1e545635d11badf0dcb7c4c7acf7d90e20c54365
hasharas <hasharamsankalpam@gmail.com>
2024-12-10 17:46:54 +0530

    Start: hero section create

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import style from "./App.module.css";
+import Hero from "./component/Hero/Hero";
 import Navbar from "./component/Navbar/Navbar";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <div className={style.App}>
         <Navbar />
+        <Hero />
       </div>
 
     </>
---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import style from './Hero.module.css'
+import HeroImg from "../../assets/hero/men.png"
+
+const Hero = () => {
+      return (
+            <section className={style.container}>
+                  <div className={style.content}>
+                        <h1 className={style.title}>
+                              Hi, I'm Hashara
+                        </h1>
+                        <p className={style.description}>
+                              I'm full satck developer ,with one year experience using react and node js and more
+                        </p>
+                        <a href="hasharamsankalpam@gmail.com" className={style.contactBtn}>Contact Me</a>
+
+                  </div>
+                  <img src={HeroImg} alt="hero" className={style.heroImg} />
+                  <div className={style.topBluer} />
+                  <div className={style.bottomBlur} />
+
+            </section>
+      );
+}
+
+export default Hero;
