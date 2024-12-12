e58e38866e91c072cff0301e629d772c0759fb27
hasharas <hasharamsankalpam@gmail.com>
2024-12-13 01:39:52 +0530

    style:about section style

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -7,34 +7,34 @@ import uiIcon from "../../assets/about/uiIcon.png";
 
 const About = () => {
       return (
-            <section className='style.container'>
-                  <h2 className="style.title">
+            <section className={style.container}>
+                  <h2 className={style.title}>
                         About
                   </h2>
-                  <div className='style.content'>
-                        <img src={men2} alt="lap with me" className='style.aboutImg' />
-                        <ul className='style.aboutItems'>
-                              <li className='style.aboutItem'>
-                                    <img src={cursorIcon} alt="" />
-                                    <div className='aboutItemText'>
+                  <div className={style.content}>
+                        <img src={men2} alt="lap with me" className={style.aboutImg} />
+                        <ul className={style.aboutItems}>
+                              <li className={style.aboutItem}>
+                                    <img src={cursorIcon} alt="curson image" />
+                                    <div className={style.aboutItemText}>
                                           <h3>Frontend Developer</h3>
                                           <p>
                                                 I'm a frontend developer with experience in bulding responsive and optimized sites
                                           </p>
                                     </div>
                               </li>
-                              <li className='style.aboutItem'>
-                                    <img src={serverIcon} alt="" />
-                                    <div className='aboutItemText'>
+                              <li className={style.aboutItem}>
+                                    <img src={serverIcon} alt="server image" />
+                                    <div className={style.aboutItemText}>
                                           <h3>Backend Developer</h3>
                                           <p>
                                                 I have experience developing fast and backend and optimized back-end systems and Apis
                                           </p>
                                     </div>
                               </li>
-                              <li className='style.aboutItem'>
-                                    <img src={uiIcon} alt="" />
-                                    <div className='aboutItemText'>
+                              <li className={style.aboutItem}>
+                                    <img src={uiIcon} alt="database image" />
+                                    <div className={style.aboutItemText}>
                                           <h3>Data Bases</h3>
                                           <p>
                                                 I have experience work with database and AWS s3..
