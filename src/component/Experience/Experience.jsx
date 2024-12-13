de92a44928875b2678266cae4db05994e7c63f0c
hasharas <hasharamsankalpam@gmail.com>
2024-12-13 22:02:59 +0530

    Start:style start in experians section

--- src/component/Experience/Experience.jsx
+++ src/component/Experience/Experience.jsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import skills from '../../data/skills.json'
+import history from '../../data/history.json';
+import style from './Experience.module.css';
 
 const Experience = () => {
       return (
-            <section>
+            <section id="experince">
+
                   <h2>
                         Experience
                   </h2>
+                  <div>
+                        <div>
+                              {skills.map((skill, id) => {
+                                    return (
+                                          <div key={id}>
+                                                <div>
+                                                      <img src={skill.imageSrc}
+                                                            alt={skill.title} />
+                                                </div>
+                                                <div>
+                                                      <p>{skill.title}</p>
+                                                </div>
+                                          </div>
+
+                                    )
+                              })}
+                        </div>
+                        <ul>
+                              {
+                                    history.map((historyItem, id) => {
+                                          return (
+                                                <li key={id}>
+
+                                                      <img src={historyItem.imageSrc} alt={`${historyItem.organisation} Logo`} />
+                                                      <div>
+                                                            <h3>
+                                                                  {`${historyItem.role}, ${historyItem.organisation}`}
+                                                            </h3>
+                                                            <p>
+                                                                  {`${historyItem.startDate} - ${historyItem.endDate}`}
+                                                            </p>
+                                                            <ul>
+                                                                  {historyItem.experiences.map((experience, id) => {
+                                                                        return (
+                                                                              <li key={id}>
+                                                                                    {experience}
+                                                                              </li>
+                                                                        )
+                                                                  })}
+                                                            </ul>
+                                                      </div>
+                                                </li>
+                                          )
+                                    })
+                              }
+                        </ul>
+                  </div>
             </section>
 
       );
